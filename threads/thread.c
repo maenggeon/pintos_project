45461160c3b4ef5f306c8ad9b1ffd97a9eb23038
LeeMG <audrjs987@naver.com>
2025-10-24 10:13:51 +0900

    inset_ready_queue_ordered

--- threads/thread.c
+++ threads/thread.c
@@ -250,7 +250,10 @@ thread_unblock (struct thread *t)
 
     old_level = intr_disable ();
     ASSERT (t->status == THREAD_BLOCKED);
-    list_push_back (&ready_list, &t->elem);
+
+    //list_push_back (&ready_list, &t->elem);
+    list_insert_ordered(&ready_list, &t->elem, compare_thread_priority, NULL);
+
     t->status = THREAD_READY;
     intr_set_level (old_level);
 }
@@ -378,7 +381,9 @@ thread_yield (void)
 
     old_level = intr_disable ();
     if (cur != idle_thread)
-        list_push_back (&ready_list, &cur->elem);
+        //list_push_back (&ready_list, &cur->elem);
+        list_insert_ordered(&ready_list, &cur->elem, compare_thread_priority, NULL);
+
     cur->status = THREAD_READY;
     schedule ();
     intr_set_level (old_level);
@@ -647,3 +652,13 @@ allocate_tid (void)
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */
 uint32_t thread_stack_ofs = offsetof (struct thread, stack);
+
+
+
+
+/* preemptive scheduling */
+compare_thread_priority(struct list_elem *a, struct list_elem *b, void *aux UNUSED)
+{
+    return list_entry(a, struct thread, elem)->priority
+        > list_entry(b, struct thread, elem)->priority;
+}
